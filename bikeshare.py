01152f48a93114109c843dcbec827521a51e1db4
Eshan Yadav <eshanyadav@gmail.com>
2020-09-08 00:00:10 +1000

    feat: Added more description lines

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 #libraries/modules consisting of additional functions to make this script and my life easier
 """commit 1"""
+"""commit 2"""
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
